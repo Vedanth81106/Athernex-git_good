c828b30c6c62147010220f27dfc24cee44900913
summi-rahman <tahseenanwer100@gmail.com>
2026-04-24 13:26:10 +0530

    Adding Title

--- frontend/src/app/Home/Home.jsx
+++ frontend/src/app/Home/Home.jsx
@@ -1,12 +1,11 @@
 "use client";
 import React from 'react'
 import PixelBlast from '@/components/PixelBlast.jsx'
-
+import Shuffle from '@/components/Shuffle';
 
 const Home = () => {
   return (
     <div className="relative h-screen w-full overflow-hidden bg-neutral-950">
-      
       {/* BACKGROUND LAYER: Animated Pixel Effects */}
       <div className="absolute inset-0 z-0">
         <PixelBlast
@@ -25,6 +24,16 @@ const Home = () => {
           edgeFade={0.25}
           transparent={true}
         />
+      </div>
+      <div className="relative z-10 flex h-full flex-col items-center justify-center gap-12 pointer-events-none">
+
+        {/* Shuffle Text: Hover effect enabled via 'pointer-events-auto' */}
+        <div className="text-5xl font-black tracking-tighter text-white md:text-7xl pointer-events-auto cursor-default">
+          <Shuffle
+            text=" Kintsugi "
+            triggerOnHover={true}
+          />
+        </div>
       </div>
     </div>
   )
