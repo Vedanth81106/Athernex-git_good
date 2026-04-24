9343e3f16fd38e25e827529feeff2047d5e99671
Vedanth81106 <vedanth81106@gmail.com>
2026-04-24 17:26:15 +0530

    Finalized dom stripper?

--- dom-stripper/src/strips/strip4_zeroSize.js
+++
@@ -1,7 +0,0 @@
-export const applyStrip4 = (elements) => {
-  return elements.filter(el => {
-    if (window.getComputedStyle(el).display === 'contents') return true;
-    const rect = el.getBoundingClientRect();
-    return !(rect.width === 0 && rect.height === 0);
-  });
-};
