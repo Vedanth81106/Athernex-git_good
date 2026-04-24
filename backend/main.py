8397352d8f5621ba07e99b7e4112e755bf3db952
summi-rahman <tahseenanwer100@gmail.com>
2026-04-25 04:11:14 +0530

    Updated frontend

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,11 @@ app = FastAPI()
 # cors
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", 'http://127.0.0.1:8000'],
+    allow_origins=[
+        "http://localhost:3000", 
+        "http://localhost:3001", 
+        "http://127.0.0.1:8000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
